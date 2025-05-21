54ff9b1fc83d39560e58b990ad181478f0fc43e1
Kim, Jeunghoon <blueming02@gmail.com>
2025-05-21 09:39:04 +0900

    five

--- src/g202114124/World.java
+++ src/g202114124/World.java
@@ -1,5 +1,11 @@
-public class world {
+package g202114124;
+
+public class World {
     public static void run() {
         System.out.println("202114124");
+    }
+
+    public static void run2() {
+    	System.out.println("hello world");
     }
 }
